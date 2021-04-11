f6db83916997700b9d600f7a5922aa90d389c605
nicolaz888 <nicolaz888@hotmail.com>
2021-04-11 12:34:58 -0500

    [FIX] - Cleaning non used resources

---
+++ js/sanite.js
@@ -0,0 +1,12 @@
+
+/**
+ * Function to hide the navbar-collapse or burger menu when clicked
+ */
+$(function () {
+    var navMain = $(".navbar-collapse"); // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
