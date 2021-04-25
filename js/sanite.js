c37f829b7d27244ea8ef5fcc239cda27491a7f9d
nicolaz888 <nicolaz888@hotmail.com>
2021-04-24 22:41:44 -0500

    [UPD] - last changes

--- js/sanite.js
+++ js/sanite.js
@@ -1,4 +1,6 @@
 
+const arrCategories = ['grid-category-kits', 'grid-category-batas', 'grid-category-accesorios', 'grid-category-guantes']
+
 /**
  * Function to hide the navbar-collapse or burger menu when clicked
  */
@@ -9,4 +11,28 @@ $(function () {
     navMain.on("click", "a:not([data-toggle])", null, function () {
         navMain.collapse('hide');
     });
+
+    // let a = 'About' + '\xa0\xa0\xa0\xa0\xa0' + '|';
+    // let nosotros = document.getElementById('nav-link-nosotros');
+    // nosotros.text = a;
+
+    // alert("The language is: " + navigator.language);
+
+    let arrCatalogoCategories = document.querySelectorAll('.catalogo-list-sanite div');
+
+    for (let i = 0; i < arrCatalogoCategories.length; i++) {
+        let category = arrCatalogoCategories[i];
+        category.onclick = () => {
+            // alert('grid-' + category.id);
+            hideCategories()
+            document.getElementById('grid-' + category.id).style.display = 'block';
+        };
+    }
+
+    function hideCategories() {
+        for (let i = 0; i < arrCategories.length; i++) {
+            const element = arrCategories[i];
+            document.getElementById(element).style.display = 'none';
+        }
+    }
 });
